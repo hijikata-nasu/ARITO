a8458de2a455908d1ed4325a7bc3e145cea3e799
cmyUE <kaitomikkumiku+github@gmail.com>
2020-09-10 16:24:31 +0900

    [add] ObentoSensor.ktを作成
    クラスの作成とメソッドの宣言のみなので中身は無いです。

---
+++ app/src/main/java/com/nitok_ict/strawberrypie/arito/ObentoSensor.kt
@@ -0,0 +1,15 @@
+package com.nitok_ict.strawberrypie.arito
+
+class ObentoSensor {
+    fun isConnected():Boolean{
+        return true
+    }
+
+    fun isBatteryLow():Boolean{
+        return true
+    }
+
+    fun isDetectLift():Boolean{
+        return true
+    }
+}
