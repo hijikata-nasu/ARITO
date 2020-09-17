3708e75db73b2c0bae73c89938736a9bd76d3f83
cmyUE <kaitomikkumiku+github@gmail.com>
2020-09-17 19:53:36 +0900

    [add] Blutoothが端末で使えるかの処理を追加

--- app/src/main/java/com/nitok_ict/strawberrypie/arito/MainActivity.kt
+++ app/src/main/java/com/nitok_ict/strawberrypie/arito/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nitok_ict.strawberrypie.arito
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,14 +9,17 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_ENABLE_BT: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //ボタン一式
         val messageRecordButton: MaterialButton = findViewById(R.id.record_message_button)
-        val messagePlayButton: MaterialButton = findViewById(R.id.message_play_button)
         val retakingButton: MaterialButton = findViewById(R.id.retaking_button)
+        val messagePlayButton: MaterialButton = findViewById(R.id.message_play_button)
 
         val messageCard: MaterialCardView = findViewById(R.id.card_message)
 
@@ -23,29 +27,11 @@ class MainActivity : AppCompatActivity() {
         val batteryLifeTextView: TextView = findViewById(R.id.textview_batterylife)
         val messagePlayTimeTextView: TextView = findViewById(R.id.textview_message_playtime)
 
-        val obentoSensor = ObentoSensor()
+        val obentoSensor = ObentoSensor(this)
 
         if(true/*メッセージが記録されているか*/) { //TODO メッセージが記録されているかの確認の処理
             messageCard.visibility = View.VISIBLE
         messagePlayTimeTextView.text = "null秒"  //TODO 再生時間を取得して格納する
-        } else {
-            messageCard.visibility = View.GONE
-        }
-
-        if (obentoSensor.isConnected()) {
-            bTConnectionTextView.setText(R.string.card_sensor_blutooth_connected)
-            if (obentoSensor.isBatteryLow()) {
-                batteryLifeTextView.setText(R.string.card_sensor_battery_life_low)
-            } else {
-                batteryLifeTextView.setText(R.string.card_sensor_battery_life_high)
-            }
-        } else {
-            bTConnectionTextView.setText(R.string.card_sensor_blutooth_unconnected)
-        }
-
-        messageRecordButton.setOnClickListener{
-            //TODO 画面遷移処理を書く
-        }
 
         messagePlayButton.setOnClickListener {
             //TODO 画面遷移処理を書く
@@ -54,5 +40,37 @@ class MainActivity : AppCompatActivity() {
         retakingButton.setOnClickListener {
             //TODO 画面遷移処理を書く
         }
+        } else {
+            messageCard.visibility = View.GONE
+        }
+
+        if (obentoSensor.isConnected()) {
+            bTConnectionTextView.setText(R.string.card_sensor_bluetooth_connected)
+            if (obentoSensor.isBatteryLow()) {
+                batteryLifeTextView.setText(R.string.card_sensor_battery_life_low)
+            } else {
+                batteryLifeTextView.setText(R.string.card_sensor_battery_life_high)
+            }
+        } else {
+            bTConnectionTextView.setText(R.string.card_sensor_bluetooth_unconnected)
+        }
+
+        messageRecordButton.setOnClickListener{
+            //TODO 画面遷移処理を書く
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK){
+
+            } else {
+                moveTaskToBack(true)
+            }
+        }
+
     }
 }
--- app/src/main/java/com/nitok_ict/strawberrypie/arito/ObentoSensor.kt
+++ app/src/main/java/com/nitok_ict/strawberrypie/arito/ObentoSensor.kt
@@ -1,12 +1,38 @@
 package com.nitok_ict.strawberrypie.arito
 
-class ObentoSensor {
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class ObentoSensor(activityContext: AppCompatActivity) {
+
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    val BLUETOOTH_SPP: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+
+    init {
+        val REQUEST_ENABLE_BT: Int = 1
+
+        //端末がBluetoothに対応していないときトーストを表示する
+        if (bluetoothAdapter == null) {
+            val toast = Toast.makeText(activityContext ,R.string.toast_bluetooth_unsupported, Toast.LENGTH_LONG)
+            toast.show()
+            activityContext.moveTaskToBack(false)
+        }
+        //Bluetoothがオフになっているとき、オンにしてもらう
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            activityContext.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+
     fun isConnected():Boolean{
         return true
     }
 
     fun isBatteryLow():Boolean{
-        return true
+        return false
     }
 
     fun isDetectLift():Boolean{
