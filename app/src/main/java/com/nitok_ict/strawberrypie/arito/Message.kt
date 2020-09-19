d582cbfdf36baa9db8e819d47386c77038336dd8
cmyUE <kaitomikkumiku+github@gmail.com>
2020-09-19 15:19:30 +0900

    [add] わかりやすいようにコメントを追記

--- app/src/main/java/com/nitok_ict/strawberrypie/arito/Message.kt
+++ app/src/main/java/com/nitok_ict/strawberrypie/arito/Message.kt
@@ -2,24 +2,24 @@ package com.nitok_ict.strawberrypie.arito
 
 class Message{
 
-    var voiceMessage: Int = 0
-    var faceData: MutableList<Face> = mutableListOf()
+    var voiceMessage: Int = 0   //音声用のメンバ
+    var faceData: MutableList<Face> = mutableListOf()   //表情データ保存用のメンバ
 
-    fun sveToFile(){
+    fun sveToFile(){        //ファイルとして保存する為の関数　TODO 実装する
     }
 
-    fun readFromFile(){
+    fun readFromFile(){     //ファイルから復元する為の関数　TODO 実装する
     }
 
-    fun deleteFile(){
+    fun deleteFile(){       //保存されているファイルを削除する関数　TODO 実装する
     }
 
-    fun sortData(){
+    fun sortData(){         //開始時間順にソートする関数
         faceData.sortBy { it.startTime }
     }
 
 }
 
-data class Face(var resID: Int,
-                var startTime: Int,
-                var endTime: Int)
+data class Face(var resID: Int,     //表情のリソースID
+                var startTime: Int, //その表情が表示され始める時間
+                var endTime: Int)   //その表情が表示され終わる時間
